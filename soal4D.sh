d8f0c16142f0729ca00cc99e00611185de42b9ac
hafidzasqalany28 <45744801+hafidzasqalany28@users.noreply.github.com>
2019-02-28 21:40:58 +0700

    Update soal4D.sh

--- soal4D.sh
+++ soal4D.sh
@@ -1,9 +1,8 @@
+
 #!/bin/bash
 
 hour=${1:0:2}
-cat "$1$2" | 
-xxd -p -c1 | 
-awk -v r=$hour '
+cat "$1$2" | xxd -p -c1 | awk -v r=$hour '
 function hex2dec(h,i,x,v){
   h=tolower(h);sub(/^0x/,"",h)
   for(i=1;i<=length(h);++i){
@@ -17,20 +16,19 @@ BEGIN { hour = strtonum(r) }
 {
 	$1 = hex2dec(0x$1)
 	if ($1 >= 65 && $1 <= 90) {
-		$1 = $1 - 65
-		$1 = ($1 - hour) % 26
-		while ($1 < 0)
-			$1 = $1 + 26
-		$1 = $1 + 65
+		$1 = $1 - 64
+		$1 = ($1 - hour) % 27
+		while ($1 < 1)
+			$1 = $1 + 27
+		$1 = $1 + 64
 	}
 	if ($1 >= 97 && $1 <= 122) {
-		$1 = $1 - 97
-		$1 = ($1 - hour) % 26
-		while ($1 < 0)
-			$1 = $1 + 26
-		$1 = $1 + 97
+		$1 = $1 - 96
+		$1 = ($1 - hour) % 27
+		while ($1 < 1)
+			$1 = $1 + 27
+		$1 = $1 + 96
 	}
 	printf("%c", $1)
 }
-' > /home/hafidzasqalany28/decrypted_"$1$2"
-
+' > /home/hafidzasqalany28/modul1/decrypted_"$1$2"
